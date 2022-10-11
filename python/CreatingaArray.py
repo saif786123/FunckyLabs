d2863e8389d3d98d224777fc16fb5f9fbcf55186
Akhilesh singh maurya <akhileshbltr2002@gmail.com>
2022-10-11 19:29:52 +0530

    Adding Elements to a Array

--- python/CreatingaArray.py
+++ python/CreatingaArray.py
@@ -1,23 +1,40 @@
 # Python program to demonstrate
-# Creation of Array
+# Adding Elements to a Array
 
 # importing "array" for array creations
 import array as arr
 
-# creating an array with integer type
+# array with int type
 a = arr.array('i', [1, 2, 3])
 
-# printing original array
-print("The new created array is : ", end=" ")
-for i in range(0, 3):
-    print(a[i], end=" ")
+
+print ("Array before insertion : ", end =" ")
+for i in range (0, 3):
+	print (a[i], end =" ")
 print()
 
-# creating an array with double type
+# inserting array using
+# insert() function
+a.insert(1, 4)
+
+print ("Array after insertion : ", end =" ")
+for i in (a):
+	print (i, end =" ")
+print()
+
+# array with float type
 b = arr.array('d', [2.5, 3.2, 3.3])
 
-# printing original array
-print("The new created array is : ", end=" ")
-for i in range(0, 3):
-    print(b[i], end=" ")
+print ("Array before insertion : ", end =" ")
+for i in range (0, 3):
+	print (b[i], end =" ")
+print()
+
+# adding an element using append()
+b.append(4.4)
+
+print ("Array after insertion : ", end =" ")
+for i in (b):
+	print (i, end =" ")
+print()
 
