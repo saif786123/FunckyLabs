d4f6e7c060b330e286a37324c70d7b474651060b
mohds <mohdsaif2711200112@gmail.com>
2022-10-19 00:12:45 +0530

    reverse an array

--- python/Iterate An Array.py
+++ python/Iterate An Array.py
@@ -1,15 +1,12 @@
-# Python program for
-# iterating over array
-import numpy as geek
+# input list
+lst = [10, 11, 12, 13, 14, 15]
+# the above input can also be given as
+# lst=list(map(int,input().split()))
+l = [] # empty list
 
-# creating an array using
-# arrange method
-a = geek.arange(9)
-
-# shape array with 3 rows
-# and 4 columns
-a = a.reshape(3, 3)
-
-# iterating an array
-for x in geek.nditer(a):
-	print(x)
+# iterate to reverse the list
+for i in lst:
+	# reversing the list
+	l.insert(0, i)
+# printing result
+print(l)
